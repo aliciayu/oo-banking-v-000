be0d73f6ecd5ca72f621208a99846af3e5bb37c7
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-20 21:43:36 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,6 @@
 class BankAccount
   attr_reader :name
-<<<<<<< HEAD
   attr_accessor :balance, :status
-=======
-  attr_reader :balance, :status
->>>>>>> 7c148479710fe2cd88874382f38de2e3e017ec7e
 
   def initialize(name)
     @name = name
@@ -13,32 +9,19 @@ class BankAccount
   end
 
   def deposit(deposit_amount)
-<<<<<<< HEAD
     self.balance += deposit_amount
-=======
-    @balance += deposit_amount
->>>>>>> 7c148479710fe2cd88874382f38de2e3e017ec7e
   end
 
   def display_balance
     "Your balance is $#{balance}."
   end
 
-<<<<<<< HEAD
   def valid?
     balance > 0 && status == "open"
   end
 
   def close_account
     self.status = "closed"
-=======
-  def close_account
-    @status = "closed"
-  end
-
-  def valid?
-    status == "open" && balance > 0
->>>>>>> 7c148479710fe2cd88874382f38de2e3e017ec7e
   end
 
 end
