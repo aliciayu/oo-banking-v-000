fbedd3c1ce85045ebde59b3909f6dadfb340134e
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-20 21:48:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,27 +1,41 @@
-class BankAccount
-  attr_reader :name
-  attr_accessor :balance, :status
+class Transfer
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
 
-  def initialize(name)
-    @name = name
-    @balance = 1000
-    @status = "open"
-  end
-
-  def deposit(deposit_amount)
-    self.balance += deposit_amount
-  end
-
-  def display_balance
-    "Your balance is $#{balance}."
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
   end
 
   def valid?
-    balance > 0 && status == "open"
+    sender.valid? && receiver.valid?
   end
 
-  def close_account
-    self.status = "closed"
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
   end
+
+  def reverse_transfer
+  if valid? && receiver.balance > amount && self.status == "complete"
+    receiver.balance -= amount
+    sender.balance += amount
+    self.status = "reversed"
+  else
+    reject_transfer
+  end
+end
+
+def reject_transfer
+  self.status = "rejected"
+  "Transaction rejected. Please check your account balance."
+end
 
 end
